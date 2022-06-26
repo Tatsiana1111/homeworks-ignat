c67cf9436ba91be64d21119c9beb9c848ab5ccc4
Tatsiana1111 <tanyashket@gmail.com>
2022-06-26 16:01:31 +0300

    hw1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import style from './Message.module.css'
 
-function Message() {
+type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+const Message: React.FC<MessageType> = (props) => {
     return (
-        <div>
-
+        <div className={style.message}>
+            <img className={style.img} src={props.avatar}/>
+            <div className={style.messageCorner}></div>
+            <div className={style.messageBlock}>
+                <div className={style.name}>{props.name}</div>
+                <div className={style.text}>{props.message}</div>
+                <div className={style.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
