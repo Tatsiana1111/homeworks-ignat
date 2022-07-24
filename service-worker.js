92ca4db60c05c6799529a3b38b76027c3cbb3b6d
Tatsiana1111 <tanyashket@gmail.com>
2022-07-24 13:25:28 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homeworks-ignat/precache-manifest.d38454d8d4c598f8e5e2d424e7fcacca.js"
+  "/homeworks-ignat/precache-manifest.02ff91975884728765ac3c4a7ae53554.js"
 );
 
 self.addEventListener('message', (event) => {
