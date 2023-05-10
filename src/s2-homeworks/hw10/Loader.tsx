c689dafe65870012ae1e7387876010447ea3fab8
Tatsiana1111 <tanyashket@gmail.com>
2023-05-10 13:12:03 +0300

    swqw

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
 import s from './Loader.module.css'
+
 
 export const Loader = () => <div className={s.loader}/>
--- src/src/s2-homeworks/hw10/Loader.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import s from './Loader.module.css'
-
-export const Loader = () => <div className={s.loader}/>
